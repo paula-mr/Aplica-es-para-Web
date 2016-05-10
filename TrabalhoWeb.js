19c91007c4b5921fcf6cc2a394566e9388f7b31d
Paula Ribeiro <paula-mr@users.noreply.github.com>
2016-05-09 23:21:57 -0300

    Add files via upload
    AGORA SIM CARALHO

--- TrabalhoWeb.js
+++ TrabalhoWeb.js
@@ -14,41 +14,54 @@ var semana;
 var hora;
 var data;
 var lista;
+//função confirma
 function confirma() {
-	alert("funciona");
-	text = document.form1.text.value;
-	senha = document.form1.senha.value;
-	op = document.form1.op.value;
-	//retornar valores da checkbox
-	//exibir();
+	confirm("Deseja confirmar?");
 }
+//função que transforma os valores da checkbox em uma única string
+function checkboxString() {
+	checked="";
+	for (i=0; i<4; i++ ) {
+		if (document.getElementById(i).checked==true) {
+			checked += "\nchk" + (i+1) + " checked";
+		}
+	}
+}
+//função gravar
 function gravar() {
-	confirma();
-	select = document.form2.select.value;
-	textarea = document.form2.textarea.value;
-	quant = document.form2.quantidade.value;
-	antes = document.form2.antes.value;
-	depois = document.form2.depois.value;
-	cor = document.form2.cor.value;
-	range = document.form2.range.value;
-	semana = document.form2.semana.value;
-	hora = document.form2.hora.value;
-	data = document.form2.horaCompleta.value;
-	//lista = document.form2.lista.value;
+	text = document.form.texto.value;
+	senha = document.form.senha.value;
+	op = document.form.op.value;
+	select = document.form.select.value;
+	checkboxString();
+	textarea = document.form.textarea.value;
+	quant = document.form.quantidade.value;
+	antes = document.form.antes.value;
+	depois = document.form.depois.value;
+	cor = document.form.cor.value;
+	range = document.form.range.value;
+	semana = document.form.semana.value;
+	hora = document.form.hora.value;
+	data = document.form.horaCompleta.value;
+	lista = document.form.lista.value;
 	exibir();
 }
+//função exibir valores
 function exibir() {
-	alert("funciona");
 	alert("Caixa de Texto: " + text +
 		"\nPassword: " + senha +
 		"\nBotão de Rádio: " + op +
-		"\nCheckbox: " + "FALTANDO" +
+		"\nCheckbox: " + checked +
 		"\nSelect: " + select +
 		"\nTextarea: " + textarea +
 		"\nQuantidade: " + quant +
 		"\nData com mínimo: " + antes +
 		"\nData com máximo: " + depois +
-		"\nCor Favorita: " + cor
+		"\nCor Favorita: " + cor +
+		"\nRange: " + range +
+		"\nSemana selecionada: " + semana +
+		"\nHora: " + hora +
+		"\nData Completa: " + data+
+		"\nLista suspensa com edição: " + lista
 		);
 }
-function limpa() {}
